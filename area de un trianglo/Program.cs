ad1d875f14fea10131fcc93d2d140e9517933f70
yoga 11e <yoga 11e@DESKTOP-OOOUNS6>
2024-02-11 17:42:28 -0400

    Agregar archivos de proyecto.

---
+++ area de un trianglo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("area de un triangulo");
+Console.WriteLine();
+
+double baseTriangulo = 5.0;
+double alturaTriangulo = 7.0;
+double area = CalcularAreaTriangulo(baseTriangulo, alturaTriangulo);
+
+Console.WriteLine("El área del triángulo es: " + area);
+    
+
+    static double CalcularAreaTriangulo(double baseTriangulo, double alturaTriangulo)
+{
+    double area = (baseTriangulo * alturaTriangulo) / 2;
+    return area;
+}
